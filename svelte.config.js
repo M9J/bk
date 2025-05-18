89c1cfe96cb33b51ca2521ce38ec3e5f2da138a7
Maneesh <mnsh.exe@gmail.com>
2025-05-18 14:38:36 +0530

    base path and misc fixes

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,18 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default {
+  preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+    }),
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/bk" : "",
+    },
     prerender: {
-      entries: ['*']
-    }
-  }
+      entries: ["*"],
+    },
+  },
 };
