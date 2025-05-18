3606abc1b3a82d42f2c1ba992a8b48ffe3b59e42
Maneesh George Johnson <mnsh.exe@gmail.com>
2025-05-19 04:19:29 +0530

    Update settings.ts

--- src/lib/util/settings.ts
+++ src/lib/util/settings.ts
@@ -22,7 +22,11 @@ export function unixEpochToVersion(timestamp: string) {
     const day = String(dt.getDate()).padStart(2, "0");
     const secsTodayTillNow = dt.getHours() * 3600 + dt.getMinutes() * 60 + dt.getSeconds();
     const secs = String(secsTodayTillNow).padStart(5, "0");
-    const version = `${year}.${month}${day}.${secs}`;
+    const hh = String(dt.getHours());
+    const mm = String(dt.getMinutes());
+    const ss = String(dt.getSeconds());
+    // const version = `${year}.${month}${day}.${secs}`;
+    const version = `${year}.${month}${day}.${hh}${mm}${ss}`;
     return version;
   }
 }
