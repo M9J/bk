f91666eaea7f8e4e286329dd4c94ca381af6e91a
Maneesh <mnsh.exe@gmail.com>
2025-05-18 20:42:59 +0530

    service worker fixes

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,8 +1,8 @@
 const CACHE_NAME = "mybookmarks-dyncache-v1";
-const folderPattern = /^\/(src|dist)?\/?(bookmarks\/)?/;
+// const folderPattern = /^\/(src|dist)?\/?(bookmarks\/)?/;
 
 // Define API endpoint to exclude from caching
-const excludedAPI = "https://api.github.com/repos/m9j/bookmarks/contents/version.json?ref=gh-pages";
+const excludedAPI = "https://api.github.com/repos/m9j/bk/contents/version.json?ref=gh-pages";
 
 self.addEventListener("fetch", (event: any) => {
   const requestURL = new URL(event.request.url);
@@ -13,26 +13,27 @@ self.addEventListener("fetch", (event: any) => {
     return;
   }
 
+  debugger;
   // Normal caching logic for other requests
-  if (folderPattern.test(requestURL.pathname)) {
-    event.respondWith(
-      caches
-        .match(event.request)
-        .then((response) => {
-          return (
-            response ||
-            fetch(event.request).then(async (fetchResponse: any) => {
-              return caches.open(CACHE_NAME).then((cache) => {
-                // console.log("CACHED:", event.request.url);
-                cache.put(event.request, fetchResponse.clone());
-                return fetchResponse;
-              });
-            })
-          );
-        })
-        .catch((error) => {
-          console.error("Fetch failed:", event.request.url, error);
-        })
-    );
-  }
+  // if (folderPattern.test(requestURL.pathname)) {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return (
+          response ||
+          fetch(event.request).then(async (fetchResponse: any) => {
+            return caches.open(CACHE_NAME).then((cache) => {
+              console.log("CACHED:", event.request.url);
+              cache.put(event.request, fetchResponse.clone());
+              return fetchResponse;
+            });
+          })
+        );
+      })
+      .catch((error) => {
+        console.error("Fetch failed:", event.request.url, error);
+      })
+  );
+  // }
 });
