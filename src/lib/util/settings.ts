e16498718d61982e3e5d83fb108528e7a2c429e7
Maneesh <mnsh.exe@gmail.com>
2025-05-24 20:24:59 +0530

    unixEpochToVersion minor string padding fix

--- src/lib/util/settings.ts
+++ src/lib/util/settings.ts
@@ -20,9 +20,9 @@ export function unixEpochToVersion(timestamp: string) {
     const year = String(dt.getFullYear());
     const month = String(dt.getMonth() + 1).padStart(2, "0");
     const day = String(dt.getDate()).padStart(2, "0");
-    const hh = String(dt.getHours());
-    const mm = String(dt.getMinutes());
-    const ss = String(dt.getSeconds());
+    const hh = String(dt.getHours()).padStart(2, "0");
+    const mm = String(dt.getMinutes()).padStart(2, "0");
+    const ss = String(dt.getSeconds()).padStart(2, "0");
     const version = `${year}` + `.` + `${month}${day}` + `.` + `${hh}${mm}${ss}`;
     return version;
   }
