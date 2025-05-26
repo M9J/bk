44630dec48d1ba41af2b0b05357b3236df070c6c
Maneesh <mnsh.exe@gmail.com>
2025-05-26 18:36:59 +0530

    clock.lambda fixes

--- static/lambdas/clock.lambda.js
+++ static/lambdas/clock.lambda.js
@@ -1,10 +1,14 @@
 export const prompt = "Current time";
 export const action = (setOutput) => {
   const dt = new Date();
-  const hh = String(dt.getHours()).padStart(2, "0");
+  let hh = 0;
+  const hours = dt.getHours();
+  if (hours > 12) hh = hours - 12;
+  hh = String(hh).padStart(2, "0");
   const mm = String(dt.getMinutes()).padStart(2, "0");
   const ss = String(dt.getSeconds()).padStart(2, "0");
-  const time = hh + ":" + mm + ":" + ss;
+  const ampm = hours / 12 > 1 ? "PM" : "AM";
+  const time = hh + ":" + mm + ":" + ss + " " + ampm;
   setOutput(time);
   let tmr1 = setTimeout(() => {
     clearTimeout(tmr1);
