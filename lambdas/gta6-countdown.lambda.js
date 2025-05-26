3f635bf99d222a57a2eb167249111f35a9f092ee
M9J <mnsh.exe@gmail.com>
2025-05-26 18:28:30 +0000

    Deploying to gh-pages from @ M9J/bk@9b41cc5a8d649e809a345bb48e45a08454c988a6 🚀

--- lambdas/gta6-countdown.lambda.js
+++ lambdas/gta6-countdown.lambda.js
@@ -1,9 +1,21 @@
-export const prompt = "Days remaning for GTA VI release";
-export const action = () => {
+export const prompt = "Days remaning till GTA VI release";
+
+export async function action(out) {
   const releaseTimestamp = "1779753600000";
   const today = new Date();
   const releaseDate = new Date(Number(releaseTimestamp));
-  let timeDifference = releaseDate - today;
-  let daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
-  return daysDifference + " days";
-};
+  const timeDifference = releaseDate - today;
+  const daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
+  const daysRemaining = daysDifference + " days";
+  const epochDiff = Number(releaseTimestamp) - Date.now();
+  const secondsRemaining = formatWithCommas(parseInt(epochDiff / 1000)) + " seconds";
+  out(`${daysRemaining} (${secondsRemaining})`);
+  let tmr = setTimeout(() => {
+    clearTimeout(tmr);
+    action(out);
+  }, 1000);
+}
+
+function formatWithCommas(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
