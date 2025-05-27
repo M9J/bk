318be591fda4b711bd9d5ee096345ce1fdb4b67a
M9J <mnsh.exe@gmail.com>
2025-05-27 12:42:56 +0000

    Deploying to gh-pages from @ M9J/bk@7c29694f1bd823bde842b903e72e527440d3e987 🚀

---
+++ lambdas/counter.lambda.js
@@ -0,0 +1,15 @@
+export const prompt = "Counter";
+
+export async function action(out) {
+  startCounter(out);
+}
+
+let count = 0;
+
+function startCounter(out) {
+  out(++count);
+  let tmr = setTimeout(() => {
+    clearTimeout(tmr);
+    startCounter(out);
+  }, 100);
+}
