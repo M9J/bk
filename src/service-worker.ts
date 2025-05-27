8de2a071d224fe97660b6ad37ee758dd8a2797fa
Maneesh George Johnson <mnsh.exe@gmail.com>
2025-05-27 18:03:20 +0530

    Update service-worker.ts

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,37 +1,26 @@
 const CACHE_NAME = "mybookmarks-dyncache-v1";
-
-// Define API endpoint to exclude from caching
 const excludedAPI = "https://api.github.com/repos/m9j/bk/contents/version.json?ref=gh-pages";
 
-self.addEventListener("fetch", (event: any) => {
+self.addEventListener("fetch", (event: FetchEvent) => {
   const requestURL = new URL(event.request.url);
 
-  // Check if the request should be excluded from caching
   if (requestURL.href === excludedAPI) {
-    event.respondWith(fetch(event.request)); // Directly fetch from network
+    event.respondWith(fetch(event.request)); // Exclude specific API request from caching
     return;
   }
 
-  // Normal caching logic for other requests
-  // event.respondWith(
-  //   fetch(event.request).catch(() => {
-  caches
-    .match(event.request)
-    .then((response) => {
-      return (
-        response ||
-        fetch(event.request).then(async (fetchResponse: any) => {
-          return caches.open(CACHE_NAME).then((cache) => {
-            console.log("CACHED:", event.request.url);
-            cache.put(event.request, fetchResponse.clone());
-            return fetchResponse;
-          });
-        })
-      );
-    })
-    .catch((error) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      // Return cached response if available, otherwise fetch from network
+      return cachedResponse || fetch(event.request).then(async (networkResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          console.log("CACHED:", event.request.url);
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    }).catch((error) => {
       console.error("Fetch failed:", event.request.url, error);
-    });
-  // })
-  // );
+    })
+  );
 });
