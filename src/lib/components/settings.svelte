ba5d467536864701281f4d191c7aad44c1443646
Maneesh <mnsh.exe@gmail.com>
2025-09-04 16:57:22 +0530

    settings grid layout

--- src/lib/components/settings.svelte
+++ src/lib/components/settings.svelte
@@ -50,59 +50,63 @@
 
 <div class="settings-container hide" id="settings-container">
   <div class="columns">
-    {#if $currentVersion || $latestVersion}
-      <div>
-        Versions:<br />
-        {#if $currentVersion}
-          <span class="monospace">{unixEpochToVersion($currentVersion)} [CURRENT]</span> <br />
-        {/if}
-        {#if $latestVersion}
-          <span class="monospace" class:is-new={$isNew}
-            >{unixEpochToVersion($latestVersion)} [LATEST]</span
-          ><br />
-        {/if}
-      </div>
-    {/if}
-  </div>
-  <div class="rows">
-    <button class="settings-button" on:click={() => checkLatestVersion()}>
-      Check for latest version
-    </button>
-    <button class="settings-button" on:click={() => hardReloadApplication()}>
-      Hard reload application
-    </button>
-  </div>
-  <br />
-  <div class="columns">
-    <div>Repo deploy status:</div>
-    <div>
-      <img
-      src="https://github.com/m9j/bk/actions/workflows/deploy.yml/badge.svg"
-      alt="Github Deploy Status"
-      />
+    <div class="columns">
+      {#if $currentVersion || $latestVersion}
+        <div>
+          Versions:<br />
+          {#if $currentVersion}
+            <span class="monospace">{unixEpochToVersion($currentVersion)} [CURRENT]</span> <br />
+          {/if}
+          {#if $latestVersion}
+            <span class="monospace" class:is-new={$isNew}
+              >{unixEpochToVersion($latestVersion)} [LATEST]</span
+            ><br />
+          {/if}
+        </div>
+      {/if}
+    </div>
+    <div class="rows">
+      <button class="settings-button" on:click={() => checkLatestVersion()}>
+        Check for latest version
+      </button>
+      <button class="settings-button" on:click={() => hardReloadApplication()}>
+        Hard reload application
+      </button>
     </div>
   </div>
-  <br />
   <div class="columns">
-    <div>Eruda: {isErudaEnabledState ? "ENABLED" : "DISABLED"}</div>
-    <div class="rows">
-      {#if !isErudaEnabledState}
-        <button class="settings-button" on:click={() => enableEruda()}>Enable Eruda</button>
-      {:else}
-        <button class="settings-button" on:click={() => disableEruda()}>Disable Eruda</button>
-      {/if}
+    <div class="columns">
+      <div>Repo deploy status:</div>
+      <div>
+        <img
+          src="https://github.com/m9j/bk/actions/workflows/deploy.yml/badge.svg"
+          alt="Github Deploy Status"
+        />
+      </div>
+    </div>
+  </div>
+  <div class="columns">
+    <div class="columns">
+      <div>Eruda: {isErudaEnabledState ? "ENABLED" : "DISABLED"}</div>
+      <div class="rows">
+        {#if !isErudaEnabledState}
+          <button class="settings-button" on:click={() => enableEruda()}>Enable Eruda</button>
+        {:else}
+          <button class="settings-button" on:click={() => disableEruda()}>Disable Eruda</button>
+        {/if}
+      </div>
     </div>
   </div>
 </div>
 
 <style>
   .settings-container {
-    display: flex;
-    flex-direction: column;
     gap: 16px;
     padding: 16px;
-    align-content: flex-start;
     color: #fff;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+    grid-auto-rows: min-content;
   }
 
   .rows {
